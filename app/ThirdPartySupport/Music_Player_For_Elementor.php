3b24999ed3a6c0ce000c4c9e4d614b2114484250
Thomas Zwirner <tzwirne@gmx.de>
2025-03-23 14:31:39 +0100

    added support for music player in elementor

--- app/ThirdPartySupport/Music_Player_For_Elementor.php
+++ app/ThirdPartySupport/Music_Player_For_Elementor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File to handle support for plugin "Responsive Addons for Elementor".
+ * File to handle support for plugin "Music Player for Elementor".
  *
  * @package play-audio-once
  */
@@ -17,14 +17,14 @@ use PlayAudioOnce\ThirdPartySupport_Base;
 /**
  * Object to handle third party support.
  */
-class Responsive_Addons_For_Elementor extends ThirdPartySupport_Base {
+class Music_Player_For_Elementor extends ThirdPartySupport_Base {
 	/**
 	 * Initialize this object.
 	 *
 	 * @return void
 	 */
 	public function init(): void {
-		add_action( 'elementor/element/rael_audio/audio_section/before_section_end', array( $this, 'add_settings' ) );
+		add_action( 'elementor/element/slide-compact-player/section_playlist/before_section_end', array( $this, 'add_settings' ) );
 		add_filter( 'elementor/widget/render_content', array( $this, 'render_setting' ), 10, 2 );
 	}
 
@@ -40,9 +40,9 @@ class Responsive_Addons_For_Elementor extends ThirdPartySupport_Base {
 			array(
 				'label'        => __( 'Play only once per session', 'play-audio-once' ),
 				'type'         => Controls_Manager::SWITCHER,
-				'label_on'     => esc_html__( 'Yes', 'wp-personio-integration' ),
-				'label_off'    => esc_html__( 'No', 'wp-personio-integration' ),
-				'return_value' => 'no',
+				'label_on'     => esc_html__( 'Yes', 'play-audio-once' ),
+				'label_off'    => esc_html__( 'No', 'play-audio-once' ),
+				'return_value' => 'yes',
 				'default'      => 'no',
 			)
 		);
@@ -57,16 +57,16 @@ class Responsive_Addons_For_Elementor extends ThirdPartySupport_Base {
 	 */
 	public function render_setting( string $content, Widget_Base $instance ): string {
 		// bail if this is not the widget.
-		if ( 'rael_audio' !== $instance->get_name() ) {
+		if ( 'slide-compact-player' !== $instance->get_name() ) {
 			return $content;
 		}
 
 		// bail if setting is not enabled.
-		if ( ! empty( $instance->get_settings( 'play_audio_once' ) ) ) {
+		if ( empty( $instance->get_settings( 'play_audio_once' ) ) ) {
 			return $content;
 		}
 
 		// add our marker on HTML output and return the resulting string.
-		return str_replace( 'rael-widget-audio', 'rael-widget-audio audio-play-once-true', $content );
+		return str_replace( 'swp-compact-player', 'swp-compact-player audio-play-once-true', $content );
 	}
 }
